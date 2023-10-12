ca51408693ee5a7c00f7f1263425000022d4f204
Oskar Magnefelt <magnefelt.oskar@gmail.com>
2023-10-13 00:58:32 +0200

    Add prototype of MainView

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
-import MainView from "./views/MainView";
-import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import React, { useContext } from "react"; // Import useContext
 import Topbar from "./views/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+import { Route, Routes } from "react-router-dom";
+import MainView from "./views/MainView";
 
 function App() {
-  // const colorMode = useContext(ColorModeContext);
   const [theme, colorMode] = useMode();
 
   return (
-    <ColorModeContext.Provider
-      value={colorMode}
-      // value={{
-      //   theme: colorMode.theme,
-      //   toggleColorMode: colorMode.toggleColorMode,
-      // }}
-    >
+    <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
           <main className="content">
             <Topbar></Topbar>
-            {/* Your other components */}
+            <Routes>
+              <Route path="/" element={<MainView />}></Route>
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
