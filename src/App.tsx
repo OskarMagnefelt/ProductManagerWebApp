bd55c26943e2db2aa2e77dd40aa60f939df036cc
Oskar Magnefelt <magnefelt.oskar@gmail.com>
2023-10-12 22:31:22 +0200

    Work on theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import MainView from "./views/MainView";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      <MainView></MainView>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <div className="app">
+          <MainView></MainView>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import MainMenuOption from "./components/MainMenuOption";
+import MainMenuOption from "../components/MainMenuOption";
 
 const MainView = () => {
   return (
